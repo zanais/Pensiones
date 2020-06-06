8d120e25939d89cfcaf65cada6efed093941781e
zanais <sony_trevi@hotmail.com>
2020-06-06 14:06:17 -0500

    add main

--- Pensiones/src/app/app-routing.module.ts
+++ Pensiones/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { MainComponent } from './main/main.component';
 
+const routes: Routes = [
+  { path: '', component: MainComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    [RouterModule.forRoot(routes)]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Pensiones/src/app/app.module.ts
+++ Pensiones/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
